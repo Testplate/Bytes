bd04e4310d729357f62bd39e1ea77e0fcac58a14
Testplate <73369601+Testplate@users.noreply.github.com>
2026-05-11 05:17:55 -1200

    Update main.py
    Better

--- source_code/main.py
+++ source_code/main.py
@@ -2,19 +2,33 @@ import os
 
 def extract_bytes(file_path):
     try:
+        file_path = file_path.strip().strip('"').strip("'")
+
         with open(file_path, 'rb') as f:
             byte_data = f.read()
-        
+
         hex_bytes = [f'0x{byte:02x}' for byte in byte_data]
-        
-        cpp_array_name = os.path.splitext(os.path.basename(file_path))[0].replace('.', '_').replace('-', '_') + "_bytes"
-        cpp_output = f"unsigned char {cpp_array_name}[] = {{\n    " + ", ".join(hex_bytes) + "\n}};"
-        
+
+        cpp_array_name = (
+            os.path.splitext(os.path.basename(file_path))[0]
+            .replace('.', '_')
+            .replace('-', '_')
+            + "_bytes"
+        )
+
+        cpp_output = (
+            f"unsigned char {cpp_array_name}[] = {{\n    "
+            + ", ".join(hex_bytes)
+            + "\n}};"
+        )
+
         return cpp_output
+
     except FileNotFoundError:
         return "Error: File not found."
     except Exception as e:
         return f"An error occurred: {e}"
+
 
 if __name__ == "__main__":
     file_path = input("Please enter the path to your file: ")
